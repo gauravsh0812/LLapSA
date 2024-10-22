2b709e4ec7f687b0c0a93888e55e9f7bf37bb761
gauravsh0812 <gauravs@arizona.edu>
2024-10-21 20:16:31 -0700

    update

--- scripts/save_sam_features.py
+++ scripts/save_sam_features.py
@@ -86,7 +86,7 @@ def main():
             clip = video[i] #(224,224)
             sam_tensor = sam_image_processor.preprocess(clip, return_tensors="pt")['pixel_values'] 
             sam_tensor = sam_tensor.half().cuda() # (1,3,1024,1024)
-            print("sam tensor shape: ", sam_tensor.shape)
+            # print("sam tensor shape: ", sam_tensor.shape)
             sam_forward_outs = sam_model(sam_tensor, output_hidden_states=True)
 
         break
