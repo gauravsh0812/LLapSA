b814ce29508e1d9a51d56343f096fa93fd39da4a
gauravsh0812 <gauravs@arizona.edu>
2024-10-23 13:22:37 -0700

    update

--- scripts/save_spatio_temporal_clip_features.py
+++ scripts/save_spatio_temporal_clip_features.py
@@ -96,7 +96,7 @@ def main():
             # try:
             video = load_video(video_path)
             video_tensor = image_processor.preprocess(video, return_tensors='pt')['pixel_values']
-            video_tensor = video_tensor.half()
+            video_tensor = video_tensor.half().cuda()
 
             image_forward_outs = vision_tower(video_tensor, output_hidden_states=True)
 
