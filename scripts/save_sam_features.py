6975fae96af13a2cc9dbdb49ad48bbdb2a2604df
gauravsh0812 <gauravs@arizona.edu>
2024-10-23 23:59:51 -0700

    update

--- scripts/save_sam_features.py
+++ scripts/save_sam_features.py
@@ -91,6 +91,7 @@ def main():
             clip = video[i] #(224,224)
             sam_tensor = sam_image_processor.preprocess(clip, return_tensors="pt")['pixel_values'] 
             sam_tensor = sam_tensor.half().cuda() # (1,3,1024,1024)
+            print(sam_tensor.shape)
             sam_forward_outs = sam_model(sam_tensor, output_hidden_states=True, return_dict=True)
             iou_score = sam_forward_outs.iou_scores #(1,1,3)
             pred_masks = sam_forward_outs.pred_masks  # torch.Size([1, 1, 3, 256, 256])
