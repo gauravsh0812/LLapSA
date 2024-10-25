4de107da5cf95b434c8cbeb298bfa8e5a5726ac1
gauravsh0812 <gauravs@arizona.edu>
2024-10-24 20:42:02 -0700

    update

--- scripts/save_vcgpt_features.py
+++ scripts/save_vcgpt_features.py
@@ -70,8 +70,8 @@ def load_and_stack_hidden_states(temp, video_id,
 
 def main():
 
-    x,y = 2500,5000
-    n= 1
+    x,y = 7500, 10000
+    n= 3
 
     args = parse_args()
     video_dir_path = args.video_dir_path
