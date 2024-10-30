e1d650bd09e74c5c842d3ac69b942882427f1e47
gauravsh0812 <gauravs@arizona.edu>
2024-10-30 16:47:40 -0700

    update

--- video_chatgpt/model/video_chatgpt.py
+++ video_chatgpt/model/video_chatgpt.py
@@ -91,18 +91,15 @@ class VideoChatGPTLlamaModel(LlamaModel):
             inputs_embeds = self.embed_tokens(input_ids)
 
         if (input_ids.shape[1] != 1 or self.training) and video_spatio_temporal_features is not None:
-            video_features = self.mm_projector(video_spatio_temporal_features)
-            print("vf shape: ", video_features.shape)
-            
+            video_features = self.mm_projector(video_spatio_temporal_features)  
+
             # ADDING Q-FORMER
             batch_size = video_features.size(0)  # Get batch size
             hidden_size = video_features.size(2)  # Get hidden size
             num_queries = self.query_embeds.size(0)  # Number of queries (should match what model expects)
             qembeds = self.query_embeds.unsqueeze(0).expand(batch_size,num_queries,hidden_size)
             video_features = self.qmodel(encoder_hidden_states=video_features,
-                                        query_embeds=qembeds).last_hidden_state
-
-            print("after q-former: ", video_features.shape)
+                                        query_embeds=qembeds).last_hidden_state  # (4, 356, 4096) same as video_features
 
             dummy_video_features = torch.zeros(video_features.shape[1], 1024, device=inputs_embeds.device,
                                                dtype=inputs_embeds.dtype)
