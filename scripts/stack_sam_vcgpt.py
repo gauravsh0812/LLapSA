a18abfd83c035175fbe964b5ff3403032df57648
gauravsh0812 <gauravs@arizona.edu>
2024-11-04 12:18:02 -0700

    update

--- scripts/stack_sam_vcgpt.py
+++ scripts/stack_sam_vcgpt.py
@@ -1,22 +1,22 @@
 from tqdm import tqdm
 import os, pickle
-import multiprocessing
 
-def main(s):
-    sam_hids = "/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/sam_hidden_states"
-    vcgpt = "/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/vcgpt_features"
+sam_hids = "/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/sam_hidden_states"
+vcgpt = "/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/vcgpt_features"
+
+def main(s):    
 
     with open(os.path.join(sam_hids,s), 'rb') as file:
-        sam_tnsr = pickle.load(file).cuda()
+        sam_tnsr = pickle.load(file)
     
     with open(os.path.join(vcgpt,s), 'rb') as file:
-        vcgpt_tnsr = pickle.load(file).cuda()
+        vcgpt_tnsr = pickle.load(file)
 
     final = (sam_tnsr, vcgpt_tnsr)
 
     with open(f"/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/stacked_sam_vcgpt/{s}.pkl", 'wb') as f:
         pickle.dump(final, f)    
 
-filenames = os.listdir("/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/sam_hidden_states")
-with multiprocessing.Pool(processes=150) as pool:
-    results = list(tqdm(pool.imap(main, filenames), total=len(filenames)))
+filenames = os.listdir("/data/shared/gauravs/llapsa/sam_vcgpt_encoded_videos/sam_hidden_states")[:10]
+for s in tqdm.tqdm(filenames):
+    main(s)
