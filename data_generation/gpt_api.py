70943bd092d92c966c1a31e92b4f152d356e41d0
gauravsh0812 <gauravs@arizona.edu>
2024-11-07 10:24:16 -0700

    update

--- data_generation/gpt_api.py
+++ data_generation/gpt_api.py
@@ -85,16 +85,15 @@ def main():
     didnot_work = 0
 
     for af in tqdm.tqdm(all_files, total=len(all_files)):
-        # try:
-        transcript = open(f"{args.input_folder}/{af}").readlines()
-        transcript = " ".join(transcript)
-        response = annotate(transcript)
-        
-        print(response)
+        try:
+            transcript = open(f"{args.input_folder}/{af}").readlines()
+            transcript = " ".join(transcript)
+            response = annotate(transcript)
+            json.dump(response, output_file)
+            output_file.write('\n')  # Add a newline after each JSON object
 
-
-        # except:
-        #     didnot_work+=1
+        except:
+            didnot_work+=1
     
 if __name__ == "__main__":
     main()
