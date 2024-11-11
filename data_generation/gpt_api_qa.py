1907933409922c221a5eeaae6cba82f8c0325e77
gauravsh0812 <gauravs@arizona.edu>
2024-11-11 11:11:56 -0700

    update

--- data_generation/gpt_api_qa.py
+++ data_generation/gpt_api_qa.py
@@ -159,88 +159,109 @@ def main():
     else:
         data = data[int(x):]
 
-    def get_response(key, text, o):
+    def get_response(key, text, o, video_id):
         response = annotate(key, text, o)
         if "```json" in response:
             response = response.replace("```json", "").replace("```", "")
         response = ast.literal_eval(response)
-        print("QUESTION: ", list(response.keys())[0])
-        print("ANSWER: ", list(response.values())[0])
+        final_response = {}
+        final_response["q"] = list(response.keys())[0]
+        final_response["a"] = list(response.values())[0]
+        final_response["video_id"] = video_id
+        final_response["type"] = key
+        
         return response
     
+    def list_to_str(items):
+        if len(items) > 1:
+            output = ", ".join(items[:-1]) + ", and " + items[-1]
+        else:
+            output = items[0] if items else ""
+        return output
+
     all_responses = []
     count = 0
     didnot_work_count = 0
     for af in tqdm.tqdm(data, total=len(data)):
-        # try:
-        video_id = af["video_id"]
-        text = af["transcript"]
+        try:
+            video_id = af["video_id"]
+            text = af["transcript"]
 
-        # print("text: ", text + "\n")
+            # print("text: ", text + "\n")
 
-        obs = af["observation"]
-        rsn = af["reason"]
-        pln = af["plan"]
-        nt = af["note"]
-        ogn = af["organs"]
-        eqp = af["equipments"]
+            obs = af["observation"]
+            rsn = af["reason"]
+            pln = af["plan"]
+            nt = af["note"]
+            ogn = af["organs"]
+            eqp = af["equipments"]
 
-        # getting QA
-        for o in obs:        
-            # print("obs: ", o + "\n")    
-            response = get_response("observation", text,o)
-            # print(response)
+            # getting QA
+            for o in obs:        
+                response = get_response("observation", text,o, video_id)
+                all_responses.append(response)
+            for r in rsn:
+                response = get_response("reason", text,r, video_id)
+                all_responses.append(response)
+            for p in pln:
+                response = get_response("plan", text,p, video_id)
+                all_responses.append(response)
+            for n in nt:
+                response = get_response("note", text,n, video_id)
+                all_responses.append(response)
+
+            # final description QA
+            details = {}
+            details['observations'] = obs
+            details['reasons'] = rsn
+            details['plans'] = pln
+            details["notes"] = nt
+            response = get_response("description", text, details, video_id)
             all_responses.append(response)
-        for r in rsn:
-            # print("rsn: ", r + "\n")
-            response = get_response("reason", text,r)
-            # print(response)
-            all_responses.append(response)
-        for p in pln:
-            # print("pln: ", p + "\n")
-            response = get_response("plan", text,p)
-            # print(response)
-            all_responses.append(response)
-        for n in nt:
-            # print("nt: ", n + "\n")
-            response = get_response("note", text,n)
-            # print(response)
-            all_responses.append(response)
+            
+            
+            # adding quantative questions
+            equipments = list_to_str(eqp)
+            organs = list_to_str(ogn)
 
-        # final description QA
-        details = {}
-        details['observations'] = obs
-        details['reasons'] = rsn
-        details['plans'] = pln
-        details["notes"] = nt
-        # print("detail: ", details)
-        response = get_response("description", text, details)
-        # print(response)
-        # print(' ')
-        all_responses.append(response)
+            if len(eqp) >=1:
+                all_responses.append(
+                    {
+                    "q": "What equipments are used in the surgical video?",
+                    "a":  f"The equipments used in the surgery are {equipments}.",
+                    "video_id": video_id,
+                    "type": "quantative",
+                    }
+                )
 
-        # print(details)
+            if len(ogn)>=1:
+                all_responses.append(
+                    {
+                    "q": "What organs are involved in the surgery?",
+                    "a":  f"The organs involved in the surgery are {organs}.",
+                    "video_id": video_id,
+                    "type": "quantative",
+                    }
+                )
 
-        # except:
-        #     didnot_work_count+=1
-        #     # exit()        
+        except:
+            didnot_work_count+=1
+
         count +=1
 
-    # print("all_responses: \n", all_responses)
-
-        # if count % 500==0:
-        #     # Write all responses to the JSON file
-        #     print(f"writing {count} results...")
-        #     output_file = open(output_json_file_path, "w")
-        #     output_file.write(json.dumps(all_responses, indent=2))
-        #     output_file.write('\n')  # Add a newline after the entire JSON object
+        if count % 500==0:
+            # Write all responses to the JSON file
+            print(f"writing {count} results...")
+            output_file = open(output_json_file_path, "w")
+            output_file.write(json.dumps(all_responses, indent=2))
+            output_file.write('\n')  # Add a newline after the entire JSON object
     
     # Write all responses to the JSON file
-    # output_file = open(output_json_file_path, "w")
-    # output_file.write(json.dumps(all_responses, indent=2))
-    # output_file.write('\n')  # Add a newline after the entire JSON object
+    output_file = open(output_json_file_path, "w")
+    output_file.write(json.dumps(all_responses, indent=2))
+    output_file.write('\n')  # Add a newline after the entire JSON object
     
-    # print("failed file numbers: ", didnot_work_count)
+    print("failed file numbers: ", didnot_work_count)
 
 if __name__ == "__main__":
     main()
