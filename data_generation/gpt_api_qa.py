494a1b5ae1377b7dd481913bcc7ac0816e15ea89
gauravsh0812 <gauravs@arizona.edu>
2024-11-22 16:02:30 -0700

    update

--- data_generation/gpt_api_qa.py
+++ data_generation/gpt_api_qa.py
@@ -182,14 +182,13 @@ def main():
 
     all_responses = []
     count = 0
+    cq,rq,nq,pq,eq,oq = 0,0,0,0,0,0
     didnot_work_count = 0
     for af in tqdm.tqdm(data, total=len(data)):
-        try:
-            video_id = af["video_id"]
+        video_id = af["video_id"]
+        if "_90sec_" in video_id:
+            count+=1
             text = af["transcript"]
-
-            # print("text: ", text + "\n")
-
             obs = af["observation"]
             rsn = af["reason"]
             pln = af["plan"]
@@ -197,76 +196,100 @@ def main():
             ogn = af["organs"]
             eqp = af["equipments"]
 
-            # getting QA
-            for o in obs:        
-                response = get_response("observation", text,o, video_id)
-                all_responses.append(response)
-            for r in rsn:
-                response = get_response("reason", text,r, video_id)
-                all_responses.append(response)
+            for o in obs:
+                cq += 1
+            for r in rsn:  
+                rq +=1
             for p in pln:
-                response = get_response("plan", text,p, video_id)
-                all_responses.append(response)
+                pq +=1
             for n in nt:
-                response = get_response("note", text,n, video_id)
-                all_responses.append(response)
-
-            # final description QA
-            details = {}
-            details['observations'] = obs
-            details['reasons'] = rsn
-            details['plans'] = pln
-            details["notes"] = nt
-            response = get_response("description", text, details, video_id)
-            all_responses.append(response)
-            
+                nq+=1
             
             # adding quantative questions
-            equipments = list_to_str(eqp)
-            organs = list_to_str(ogn)
-
             if len(eqp) >=1:
-                all_responses.append(
-                    {
-                    "q": "What equipments are used in the surgical video?",
-                    "a":  f"The equipments used in the surgery are {equipments}.",
-                    "video_id": video_id,
-                    "type": "quantative",
-                    }
-                )
-
-            if len(ogn)>=1:
-                all_responses.append(
-                    {
-                    "q": "What organs are involved in the surgery?",
-                    "a":  f"The organs involved in the surgery are {organs}.",
-                    "video_id": video_id,
-                    "type": "quantative",
-                    }
-                )
-
-        except Exception as e:
-            error_message = str(e)
-            print(f"Error processing file {e}")
-            if "Rate limit reached" in error_message or "You exceeded your current quota" in error_message:
-                print("Rate limit reached. Stopping execution.")
-                sys.exit(1)  # Or re-raise the exception if preferred
-
-        count +=1
-
-        if count % 50==0:
-            # Write all responses to the JSON file
-            print(f"writing {count} results...")
-            output_file = open(output_json_file_path, "w")
-            output_file.write(json.dumps(all_responses, indent=2))
-            output_file.write('\n')  # Add a newline after the entire JSON object
+                eq+=1
+            if len(ogn) >=1:
+                oq +=1
     
-    # Write all responses to the JSON file
-    output_file = open(output_json_file_path, "w")
-    output_file.write(json.dumps(all_responses, indent=2))
-    output_file.write('\n')  # Add a newline after the entire JSON object
+    print(count+oq+eq+pq+nq+rq+cq)
+    #         try:
+    #             text = af["transcript"]
+    #             obs = af["observation"]
+    #             rsn = af["reason"]
+    #             pln = af["plan"]
+    #             nt = af["note"]
+    #             ogn = af["organs"]
+    #             eqp = af["equipments"]
+
+    #             # getting QA
+    #             for o in obs:        
+    #                 response = get_response("observation", text,o, video_id)
+    #                 all_responses.append(response)
+    #             for r in rsn:
+    #                 response = get_response("reason", text,r, video_id)
+    #                 all_responses.append(response)
+    #             for p in pln:
+    #                 response = get_response("plan", text,p, video_id)
+    #                 all_responses.append(response)
+    #             for n in nt:
+    #                 response = get_response("note", text,n, video_id)
+    #                 all_responses.append(response)
+
+    #             # final description QA
+    #             details = {}
+    #             details['observations'] = obs
+    #             details['reasons'] = rsn
+    #             details['plans'] = pln
+    #             details["notes"] = nt
+    #             response = get_response("description", text, details, video_id)
+    #             all_responses.append(response)
+                
+    #             # adding quantative questions
+    #             equipments = list_to_str(eqp)
+    #             organs = list_to_str(ogn)
+
+    #             if len(eqp) >=1:
+    #                 all_responses.append(
+    #                     {
+    #                     "q": "What equipments are used in the surgical video?",
+    #                     "a":  f"The equipments used in the surgery are {equipments}.",
+    #                     "video_id": video_id,
+    #                     "type": "quantative",
+    #                     }
+    #                 )
+
+    #             if len(ogn)>=1:
+    #                 all_responses.append(
+    #                     {
+    #                     "q": "What organs are involved in the surgery?",
+    #                     "a":  f"The organs involved in the surgery are {organs}.",
+    #                     "video_id": video_id,
+    #                     "type": "quantative",
+    #                     }
+    #                 )
+
+    #         except Exception as e:
+    #             error_message = str(e)
+    #             print(f"Error processing file {e}")
+    #             if "Rate limit reached" in error_message or "You exceeded your current quota" in error_message:
+    #                 print("Rate limit reached. Stopping execution.")
+    #                 sys.exit(1)  # Or re-raise the exception if preferred
+
+    #         count +=1
+
+    #         if count % 50==0:
+    #             # Write all responses to the JSON file
+    #             print(f"writing {count} results...")
+    #             output_file = open(output_json_file_path, "w")
+    #             output_file.write(json.dumps(all_responses, indent=2))
+    #             output_file.write('\n')  # Add a newline after the entire JSON object
+        
+    # # Write all responses to the JSON file
+    # output_file = open(output_json_file_path, "w")
+    # output_file.write(json.dumps(all_responses, indent=2))
+    # output_file.write('\n')  # Add a newline after the entire JSON object
     
-    print("failed file numbers: ", didnot_work_count)
+    # print("failed file numbers: ", didnot_work_count)
 
 if __name__ == "__main__":
     main()
