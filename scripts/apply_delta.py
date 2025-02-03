6acfdc65618f1bda944cad36045371556a6e179c
gauravsh0812 <gauravs@arizona.edu>
2025-02-02 22:59:14 -0800

    updatig LLapSA

--- scripts/apply_delta.py
+++ scripts/apply_delta.py
@@ -7,7 +7,7 @@ import argparse
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from longvlm.model.longvlm import LongVLMForCausalLM
+from llapsa.model.llapsa import LLapSAForCausalLM
 
 
 def apply_delta(base_model_path, target_model_path, delta_path):
@@ -16,13 +16,15 @@ def apply_delta(base_model_path, target_model_path, delta_path):
         base_model_path, torch_dtype=torch.float16, low_cpu_mem_usage=True)
 
     print("Loading delta")
-    delta = LongVLMForCausalLM.from_pretrained(delta_path, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    delta = LLapSAForCausalLM.from_pretrained(delta_path, torch_dtype=torch.float16, low_cpu_mem_usage=True)
     delta_tokenizer = AutoTokenizer.from_pretrained(delta_path)
 
     print("Applying delta")
+    # added by gauravs
     for name, param in tqdm(delta.state_dict().items(), desc="Applying delta"):
         if name not in base.state_dict():
-            assert name in ['model.mm_projector.weight', 'model.mm_projector.bias'], f'{name} not in base model'
+            assert name in ['model.mm_projector.weight', 'model.mm_projector.bias',
+                            "model.mm2.weight", "model.mm2.bias"], f'{name} not in base model'
             continue
         if param.data.shape == base.state_dict()[name].shape:
             param.data += base.state_dict()[name]
