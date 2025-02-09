aaa67edb454a4d4cd22de4ae6843fa239ec8455a
gauravsh0812 <gauravs@arizona.edu>
2025-02-08 21:47:59 -0700

    update

--- scripts/save_features_sp_attn.py
+++ scripts/save_features_sp_attn.py
@@ -111,8 +111,8 @@ def main():
             image_forward_outs = vision_tower(video_tensor, output_hidden_states=True)
 
         if not os.path.exists(f"{clip_feat_path_local}/{video_id}.pkl"):
-            attention_weights = torch.nn.functional.softmax(features, dim=-1)
-            weighted_features = features * attention_weights
+            attention_weights = torch.nn.functional.softmax(image_forward_outs, dim=-1)
+            weighted_features = image_forward_outs * attention_weights
             pooled_features = torch.nn.functional.adaptive_avg_pool1d(weighted_features, output_size=256)
             video_features[video_id] = merge_tokens(pooled_features, r_merge_list=[2880, 1440, 720, 360, 180, 90, 40]).detach().cpu().numpy().astype("float16")  # [1280, 640, 320, 160, 80, 40, 10]  
             
