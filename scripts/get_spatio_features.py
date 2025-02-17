858d643e9a847a8356b6e05b927362469edf3846
gauravsh0812 <gauravs@arizona.edu>
2025-02-17 14:46:17 -0700

    update

---
+++ scripts/get_spatio_features.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os, pickle, tqdm
+
+def get_spatio_temporal_features(features, num_temporal_tokens=100):
+    t, s, c = features.shape
+
+    temporal_tokens = np.mean(features, axis=1)
+    padding_size = num_temporal_tokens - t
+    if padding_size > 0:
+        temporal_tokens = np.pad(temporal_tokens, ((0, padding_size), (0, 0)), mode='constant')
+
+    spatial_tokens = np.mean(features, axis=0)
+    sp_features = np.concatenate([temporal_tokens, spatial_tokens], axis=0)
+
+    return sp_features
+
+if __name__ == "__main__":
+    L = os.listdir("/data/shared/gauravs/llapsa/surgical_tutor/llapsa/dino/dino_features/")
+    for i in tqdm.tqdm(L, total=len(L)):
+        with open(f"/data/shared/gauravs/llapsa/surgical_tutor/llapsa/dino/dino_features/{i}","rb") as f:
+            tnsr = pickle.load(f)
+        sp_tmp = get_spatio_temporal_features(tnsr)
+        with open(f"/data/shared/gauravs/llapsa/surgical_tutor/llapsa/dino/dino_sp_temp_features/{i}","wb") as f:
+            pickle.dump(sp_tmp, f)
