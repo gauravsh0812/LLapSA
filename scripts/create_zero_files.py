32c9c19ab31f861c0566bce5308b36d4cc78ca89
gauravsh0812 <gauravs@arizona.edu>
2025-02-18 01:01:53 -0700

    update

--- scripts/create_zero_files.py
+++ scripts/create_zero_files.py
@@ -107,28 +107,28 @@ def main():
             id = item["video_id"]
             text = item["input_text"]
             # print(id)
-            # try:
-            if "60sec" in id:
-                response = annotate_current(text)
-            elif "45sec" in id:
-                # print(text)
-                response = annotate_future(text)
+            try:
+                if "60sec" in id:
+                    response = annotate_current(text)
+                elif "45sec" in id:
+                    # print(text)
+                    response = annotate_future(text)
 
-            for i,r in enumerate(response):
-                qtns.append(
-                    {"video_name":id,
-                    "question":r["Q"],
-                    "question_id":f"q_{id}_{i}"}
-                )
-                ans.append(
-                    {"video_name":id,
-                    "answer":r["A"],
-                    "question_id":f"q_{id}_{i}"
-                    }
-                )
+                for i,r in enumerate(response):
+                    qtns.append(
+                        {"video_name":id,
+                        "question":r["Q"],
+                        "question_id":f"q_{id}_{i}"}
+                    )
+                    ans.append(
+                        {"video_name":id,
+                        "answer":r["A"],
+                        "question_id":f"q_{id}_{i}"
+                        }
+                    )
 
-            # except:
-            #     pass
+            except:
+                pass
             
         else:
             break
